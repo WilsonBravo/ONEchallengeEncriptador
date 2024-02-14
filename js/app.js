1cd08a801da8fc038388d0eaa114089fb372c82f
Wilson Bravo <bravoc.wilson@gmail.com>
2024-02-13 22:02:51 -0500

    cambios logica js, agregar boton pegar, cambios botones

--- js/app.js
+++ js/app.js
@@ -4,6 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
     // Variables --------------------------------------------------------------------------------
     let inputText = ''
     let outputText = ''
+
+    let copyText = ''
+    let pasteText = ''
+
+    let fixedKey = '}8[+soa@{Z:&4=t?S*oX*,DID6P19izKm0wZz,GMS;=#MI3cMM0R[sP63%LV_Alqz*%IiAueGn$6@},_<8X^!w0LTsud@c5$A>=7FA#w)cV(efpcmrM,-JD6vHQ;PPI#%nU2u,qbZsdE9T}<kUusa)-Ju]mp+8WP$A=0=Gs!ba{2Tg}sjR@)S|Tb%Lcj]$=K!^vhK*%J.IAxp6)EZTgVLR=F3Gy!X;6U&hXP^EU5H[$b*+&mdNkwCRJX*g(tSNKjonYSP9+r#pZ^jDk453ZJ/h7_r}$lj3uPT&ufsw5lP+I_lN^w7(x?v!-AC4@(0]vJ1h+ruBNK8lJ]lv<VhZlwP&$slWkh#(L98P-eedSHFx>6ALOik|Vb!mS4,_bD$+?nCSsN-57>ryQ=)HNi*;0P9ZbTOc<sXLGNeP{hZV%CgeOXp[wIPZ<]_2=&V_{nhR2!hJQ#ig?j!8J]Gztcy@5haOSfYGUxUw.lS&_3Rr4N7B:8e0+S/0r!qAmof9+o<RuN-Z2R|2tAeMG624{DxBVR*QziNh4?nl0mPQ)(SynU)k9IFZ4uA*33pK.twqjV[vU9twB;]lVeG,lJdTKBs})L3P17ijjTjsig2ybf$TU;H]n&w5G{[N0g}&uBJW0&e16kP#ek]5HIC+k=y&@1CH^4#RFlpAc^/Y#7]zkc9u*xT>k^|>kwLtr8SB-hYj%*yVBHh!.edGEn0:&OwMl/2@Yao3;sPyCx(<7d8mZYRJl[0$K,ICi#oDeGByk8NG@v+bMwMglxa)So=51xu?6c=E1o>]ispw_h(-:D(wiB$tNew<E]L:7OFa/WzsuRO5N6cdt*2t#KG?K:7MXT+BXLA/SDJv9UG1@kKqV:t_d=rM3!_kU]hh,2DtfsSn7yJB=K+BrOk6g&L|K6w3M9ns:U]*h_KLkJrOwft<4YoNCo+*LOq&2/b<[QSIATw5j}^R<O&SkUz^@K=u4uY_0J%tvOas-uT^Hyh!ISJ4kM96&S3Sn['
     let key = ''
 
     // Expresiones Regulares --------------------------------------------------------------------
@@ -63,7 +68,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         else {
             // Generar clave
-            key = getRandomKey(inputText.length);
+            // key = getRandomKey(inputText.length);
+            key = fixedKey.slice(0, inputText.length);
             // console.log(`Mi palabra clave: ${key}`)
             // Cifrar Texto
             outputText = cifrarTexto(inputText, key);
@@ -99,7 +105,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Funcion para copiar texto
     document.getElementById('botonCopiar').onclick = function () {
         // Obtener el campo de texto
-        var copyText = document.getElementById('output-text');
+        copyText = document.getElementById('output-text');
 
         // Copiar el valor del campo
         navigator.clipboard.writeText(copyText.value);
@@ -109,5 +115,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
         return
     };
+
+    // Funcion para pegar texto
+    document.getElementById('botonPegar').onclick = function () {
+        // Obtener el campo de texto
+        navigator.clipboard.readText()
+            .then(function(textoCopiado) {
+                pasteText = textoCopiado;
+
+                // Hacer algo con el texto (puedes mostrarlo en la consola o en otro lugar)
+                console.log('Texto copiado:', textoCopiado);
+                document.getElementById('input-text').value = pasteText;
+            })
+
+        return
+    };
+
+    // Crear cadena de texto clave
+    // let fixedKey = getRandomKey(1000); 
+    // console.log(`La cadena de texto clave es: ${fixedKey}`)
 
 });
